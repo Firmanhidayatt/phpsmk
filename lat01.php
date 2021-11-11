237db82e5eda1f89e24486e5dc0d6ed95db928f9
Firmanhidayatt <91240832+Firmanhidayatt@users.noreply.github.com>
2021-11-11 13:21:46 +0700

    Add files via upload

---
+++ lat01.php
@@ -0,0 +1,21 @@
+<?php 
+
+    echo "saya belajar php";
+
+    echo '<br>';
+
+    echo 'saya siswa SMK';
+
+    echo '<h1 style="background-color:pink"> belajar PHP itu mudah</h1>
+          <p style="background-color:red"> saya suka belajar program</p>
+    ';
+
+    echo 'saya belajar php'.' <br>';
+
+    echo 2019 + 2;
+
+    
+
+
+
+?>
